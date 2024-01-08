6781508eef7214e2240a5f19849d592b6b755f35
Romeo Noveanre <noveanrer@gmail.com>
2024-01-08 13:22:14 +0700

    add metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,8 +4,14 @@ import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices'
+}
 
 export default async function Page({ searchParams }: {
   searchParams?: {
